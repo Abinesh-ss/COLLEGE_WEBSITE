c4f5d5d4bfa712f6a03a45c1523b1aeb6c49475c
Abinesh s <119126115+Abinesh-ss@users.noreply.github.com>
2023-08-07 13:47:52 +0530

    Add files via upload

---
+++ CLG_WEBPAGE/hscript.js
@@ -0,0 +1,20 @@
+//   <script src="https://unpkg.com/scrollreveal"></script>
+//    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+//    <script src="script.js"></script>
+// INTHA 3 LINEYAUM UNGA HTML FILE LA ADD PANIKOONGA ANTHA JS FILE MATTUM UNGA WISH PADI NAME CHANGE PANIKOONGA
+// APROM INTHA KEELA ULLA JS LINES AA APIDI YA POTUTU UNGALUKU ENTA DIRECTION REVEAL AAGANUMOO ANTHA DIRECTIONLA UNGA "CLASS NAME" A PODUNGA
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:1000,
+    delay:100
+});
+ 
+
+ ScrollReveal().reveal('.navbar-brand,.btn btn-secondary,.carousel-inner,.btn,.pay-btn,.telegram,.events,.footer,.list-contents,.num',{origin: 'top'});
+ ScrollReveal().reveal('.btn btn-danger btn-sm,.evt-date,.news-head,.card-text,.loc', {origin: 'bottom'});
+ ScrollReveal().reveal('.container-box,.foot-contact,.carousel-inner,.circle,.pay-head,.card-group,.h2,.title,.links,.container,.pay-head', {origin: 'left'});
+ ScrollReveal().reveal('.box,.news,.pay-link,.box,.right d-flex justify-content-around mx-auto,.profiles,.events-right,.right d-flex justify-content-around mx-auto,.logo1,.card,.foot-contact,.details', {origin: 'right'});
+ 
+ 
